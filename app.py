57f3e8c9aa2aea65f719a9e978cc97db04c31c03
mahd <mahdm321@gmail.com>
2026-04-20 12:12:43 +0100

    Fix WebRTC issue by adding TURN server

--- app.py
+++ app.py
@@ -84,11 +84,25 @@ if mode == "Webcam (Live)":
         key="mask-detection",
         mode=WebRtcMode.SENDRECV,
         video_processor_factory=FaceMaskTransformer,
-        rtc_configuration={"iceServers": [
-            {"urls": ["stun:stun.l.google.com:19302"]}]},
+        rtc_configuration={
+            "iceServers": [
+                {"urls": ["stun:stun.l.google.com:19302"]},
+                {
+                    "urls": ["turn:openrelay.metered.ca:80"],
+                    "username": "openrelayproject",
+                    "credential": "openrelayproject",
+                },
+                {
+                    "urls": ["turn:openrelay.metered.ca:443"],
+                    "username": "openrelayproject",
+                    "credential": "openrelayproject",
+                },
+            ]
+        },
         media_stream_constraints={"video": True, "audio": False},
         async_processing=True,
     )
+
 elif mode == "Upload Image":
     st.title("Image Detection")
     file = st.file_uploader("Upload Image", type=['jpg', 'png', 'jpeg'])
